f1dbc5581f5e6e07c2b59a1653b4e2a40c50a350
joaonota <joaonota23@gmail.com>
2021-07-20 12:31:19 +0200

    versao 11.1

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,5 @@
 import 'package:flutter/material.dart';
 import 'package:xilhamalisso/Autenticacao/AuteticacaoUser/AutenticaUser.dart';
-import 'package:xilhamalisso/chamada/Tela_Chamada.dart';
 
 void main() {
   runApp(MyApp());
